94c9e1eb8d2d30ae8d7dcb23e27a8a4d62abf4ba
Moises Santos <Moises Santos@DESKTOP-OCUQ1EB>
2020-03-01 16:38:49 -0300

    vei meu deus

--- src/treinamento/Treinamento.java
+++ src/treinamento/Treinamento.java
@@ -16,7 +16,7 @@ public class Treinamento {
      */
     public static void main(String[] args) {
         
-        
+    System.out.println("meudeus");   
     System.out.println("eai");
         // TODO code application logic 
         
